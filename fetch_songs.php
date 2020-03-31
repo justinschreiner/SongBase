0340f715e757fcd6c22e5e0a9b3ab132a84d5d90
zackkhaz1 <54860278+zackkhaz1@users.noreply.github.com>
2020-03-31 16:28:11 -0500

    Add files via upload
    Users can now search for songs by title, next plan is to allow users to search an album and get all songs in that album

--- fetch_songs.php
+++ fetch_songs.php
@@ -1,26 +1,76 @@
 <?php
-$songname = $_POST['name'];
+error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 $mysqli = new mysqli("mysql.eecs.ku.edu", "jschreiner", "pass123", "jschreiner");
+$songname = $_POST['inputName'];
+//$tempo = $_POST['tempoComparison'];
 
-if(empty($songname))
+  if($mysqli->connect_errno)
+    {
+      printf("Connection to database failed %s\n", $mysqli->connect_error);
+      exit();
+    }
+  else
   {
-    echo "No song name entered\n";
-    header('Location: songs.html');
-  }
+    $query = "SELECT title, ALBUMS.name
+              FROM SONGS, ALBUMS
+              WHERE (title = '".$songname."'
+              OR name = '".$songname."')
+              AND ALBUMS.a_id = SONGS.a_id";
 
-if($mysqli->connect_errno)
-  {
-    printf("Connection to database failed %s\n", $mysqli->connect_error);
-    exit();
-  }
-else
-{
-  $query = "SELECT title FROM SONGS WHERE TITLE = '".$songname."'";
-    if($mysqli->query($query) == TRUE)
+    $result = $mysqli->query($query);
+    if($result->num_rows > 0)
       {
-        echo "Song searched for\n";
+        while($row = $result->fetch_assoc())
+          {
+            $song = $row["title"];
+            $album = $row["name"];
+          }
       }
-}
+    else if($result->num_rows <= 0 || empty($songname))
+    {
+      echo "No songs matching that criteria, redirecting to search page. <br>";
+      $song = "
+      ";
+      $album = "
+       ";
+      header('Refresh: 3; songs.html');
+    }
+  }
 
 
 ?>
+
+<!doctype html>
+  <html>
+    <head>
+        <Title> Search results </title>
+          <style>
+          body{
+                background-color: white;
+                border-radius: 30px;
+                padding: 1.5%;
+                margin-top: 50px;
+                margin-left: 30%;
+                margin-right: 30%;
+              }
+
+          table, th, td
+          {
+              border-collapse: collapse;
+              border: 1px solid black;
+          }
+          </style>
+        </head>
+        <body>
+          <center>
+          <table>
+              <tr>
+                <th> Song title </th>
+                <th> Album name </th>
+              </tr>
+              <tr>
+                <td> <?php echo $song;?> </td>
+                <td> <?php echo $album;?> </td>
+              </tr>
+            </table>
+          </center>
