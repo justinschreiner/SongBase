9bfcbde97e5174940b7c82f82da9f2a6ba122ae5
zackkhaz1 <54860278+zackkhaz1@users.noreply.github.com>
2020-04-06 15:34:56 -0500

    Creating new users works, with password hash
    Attempting to create a user with missing fields, nonmatching passwords, or existing user currently just refresh the page. Need to create a javascript popup window for it

---
+++ createUser.php
@@ -0,0 +1,47 @@
+<?php
+error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+$username = $_POST['username'];
+$password = $_POST['password1'];
+$verifyPass = $_POST['password2'];
+$mysqli = new mysqli("mysql.eecs.ku.edu", "jschreiner", "pass123", "jschreiner");
+if($mysqli->connect_errno)
+  {
+    printf("Connection to database failed %s\n", $mysqli->connect_error);
+    exit();
+  }
+if(!empty($username) && !empty($password))
+  {
+    $testQuery = "SELECT username
+                  FROM USERS
+                  WHERE USERS.username = '".$username."'";
+    $testResult = $mysqli->query($testQuery);
+    if($testResult->num_rows > 0)
+      {
+        header('Refresh: .1; create.html');
+        //echo '<script>existingUser()</script>'
+      }
+    else if($password == $verifyPass && !empty($username) && !empty($password))
+      {
+        $password = password_hash($password, PASSWORD_DEFAULT);
+        $insertQuery = "INSERT INTO USERS(username,password) VALUES ('$username', '$password')";
+        if($mysqli->query($insertQuery) == TRUE)
+          {
+            header('Refresh: .1; home.html');
+          }
+      }
+    else
+    {
+      header('Refresh: .1; create.html');
+
+    }
+  }
+else
+{
+  header('Refresh: .1; create.html');
+//  echo '<script>emptyField()</script>'
+
+}
+
+
+
+ ?>
