59f622a89dd2c097b15d1947a03d62049693abf7
Justin Schreiner <jschreiner22@gmail.com>
2020-05-03 13:29:03 -0500

    allowing single and double quotation marks on user input to create playlists

--- create-playlist.php
+++ create-playlist.php
@@ -5,16 +5,20 @@ $uid = $_SESSION['uid'];
 $pname = $_POST['createPlaylistName'];
 $pdesc = $_POST['createPlaylistDescription'];
 $mysqli = new mysqli("mysql.eecs.ku.edu", "jschreiner", "pass123", "jschreiner");
-if($mysqli->connect_errno)
-  {
-    printf("Connection to database failed %s\n", $mysqli->connect_error);
-    exit();
-  }
-if(!empty($pname))
-  {
+if ($mysqli->connect_errno) {
+  printf("Connection to database failed %s\n", $mysqli->connect_error);
+  exit();
+}
+if (!empty($pname)) {
+  //allow input to have '
+  $pname = str_replace("'", "\'", $pname);
+  $pdesc = str_replace("'", "\'", $pdesc);
 
-      $query = "INSERT INTO PLAYLISTS (u_id, name, description) VALUES ($uid, '$pname', '$pdesc')";
-      $mysqli->query($query);
-      header('Refresh: .1; songs.php');
-  }
- ?>
+  //allow input to have "
+  $pname = str_replace('"', '\"', $pname);
+  $pdesc = str_replace('"', '\"', $pdesc);
+
+  $query = "INSERT INTO PLAYLISTS (u_id, name, description) VALUES ($uid, '$pname', '$pdesc')";
+  $mysqli->query($query);
+  header('Refresh: .1; songs.php');
+}
